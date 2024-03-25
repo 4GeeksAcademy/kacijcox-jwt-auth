13aad8de21555b417b38d148412c365176eef322
kacijcox <145575906+kacijcox@users.noreply.github.com>
2024-03-25 19:16:37 +0000

    final commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,33 +1,75 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
-from flask import Flask, request, jsonify, url_for, Blueprint, current_app
+from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_cors import CORS
 
-#create flask app
+#new
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+
 api = Blueprint('api', __name__)
 
+#new
+#app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this "super secret" with something else!
+#jwt = JWTManager(app)
 
+@api.route('/hello', methods=['POST', 'GET'])
+def handle_hello():
 
-# Allow CORS requests to this API
-CORS(api)
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
 
+    return jsonify(response_body), 200
 
-# Create a route to authenticate your users and return JWTs. The
-# create_access_token() function is used to actually generate the JWT.
-@api.route("/token", methods=["POST", "GET"])
+#login
+@api.route('/token', methods=['POST'])
 def create_token():
-    email = request.json.get("email", None)
+    username = request.json.get("username", None)
     password = request.json.get("password", None)
-    if email != "test" or password != "test":
-        return jsonify({"msg": "Bad email or password"}), 401
+    # Query your database for username and password
+    user = User.query.filter_by(email=username, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id }) ,200
 
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token)
+#create user
+@api.route('/signup', methods=['POST'])
+def createUser():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    is_active = request.json.get("is_active", None)
+    user = User.query.filter_by(email=username).first()
+    if user != None:
+        return jsonify({"msg": "username exists"}), 401
+    
+    user = User(email=username, password=password, is_active=is_active)
+    db.session.add(user)
+    db.session.commit()
+    
+    response_body = {
+        "msg": "User successfully added "
+    }
 
+    return jsonify(response_body), 200
+
+#private route
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()    
+    user = User.query.get(current_user_id)
+
+    if user == None:
+        response_body = {
+            "msg": "Please login to continue"
+        }
+        return jsonify(response_body)
+    
+    return jsonify({"id": user.id, "username": user.email }), 200
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,8 +5,10 @@ import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
-import { Login } from "./pages/login";
 import { Single } from "./pages/single";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Private from "./pages/Private.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -28,9 +30,11 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
-                        <Route element={<Login />} path="/login" />
                         <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Private />} path="/private" />
+                        <Route element={<h1>Not found!</h1>} path="*"/>
                     </Routes>
                     <Footer />
                 </ScrollToTop>
